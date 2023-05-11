9896368df099f8685c49569fd73a3a01b96af308
pushs <pushs@PUSHPA>
2023-05-11 16:07:45 +0530

    loans project commit

---
+++ loans/src/main/java/com/demo/loans/controller/LoansController.java
@@ -0,0 +1,31 @@
+package com.demo.loans.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.loans.model.Customer;
+import com.demo.loans.model.Loans;
+import com.demo.loans.repository.LoansRepository;
+
+@RestController
+public class LoansController {
+
+	@Autowired
+	private LoansRepository loansRepository;
+
+	@PostMapping("/myLoans")
+	public List<Loans> getLoansDetails(@RequestBody Customer customer) {
+		List<Loans> loans = loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
+		if (loans != null) {
+			return loans;
+		} else {
+			return null;
+		}
+
+	}
+
+}
---
+++ loans/src/main/java/com/demo/loans/model/Customer.java
@@ -0,0 +1,9 @@
+package com.demo.loans.model;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+	private int customerId;
+
+}
